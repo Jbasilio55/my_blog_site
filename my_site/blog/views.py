1ddfa4a62369b5a7930b0bad3eed536bfc3d7942
Jbasilio55 <j.l.basilio55@gmail.com>
2024-07-19 11:07:44 -0400

    added view CSS and templates

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,12 +1,90 @@
+from datetime import date
 from django.shortcuts import render
+
+all_posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Jorge",
+        "date": date(2022, 7, 22),
+        "title": "Mountain Hiking",
+        "excerpt": """
+            There is nothing better that witnessing nature up close and personal! 
+            The views are absolutely stunning and I was not prepared for what happened while I was enjoying the view.
+        """,
+        "content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident blanditiis 
+            reiciendis facilis quisquam fugit sit ea saepe ipsam optio qui! Accusantium, saepe veniam 
+            corporis voluptate praesentium blanditiis ipsum ducimus at.
+            
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident blanditiis 
+            reiciendis facilis quisquam fugit sit ea saepe ipsam optio qui! Accusantium, saepe veniam 
+            corporis voluptate praesentium blanditiis ipsum ducimus at.
+            
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident blanditiis 
+            reiciendis facilis quisquam fugit sit ea saepe ipsam optio qui! Accusantium, saepe veniam 
+            corporis voluptate praesentium blanditiis ipsum ducimus at.
+        """,
+        
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.jpg",
+        "author": "Maximilian",
+        "date": date(2022, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpg",
+        "author": "Maximilian",
+        "date": date(2020, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+            aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+            velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+]
+
+def get_date(post):
+    return post["date"]
 
 # Create your views here.
 
 def starting_page(request):
-    pass
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 def posts(request):
-    pass
+    return render(request, "blog/all-posts.html")
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    return render(request, "blog/post-detail.html")
