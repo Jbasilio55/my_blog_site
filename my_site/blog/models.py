e6ceb6eb18850c944b32111124df8d3467545547
Jbasilio55 <j.l.basilio55@gmail.com>
2024-07-23 18:45:58 -0400

    adjusting admin layouts and created superuser

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -5,10 +5,20 @@ from django.core.validators import MinLengthValidator
 
 class Tag(models.Model):
     caption = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.caption
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
+    
+    def fullname(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        return self.fullname()
+    
 class Post(models.Model):
     title = models.CharField(max_length=100)
     excerpt = models.CharField(max_length=200)
@@ -16,5 +26,5 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="post")
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
