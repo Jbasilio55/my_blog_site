46d4a8d058298a4f7fa9943baad29df68ffc4029
Jbasilio55 <j.l.basilio55@gmail.com>
2024-07-25 22:36:42 -0400

    changing models so that user can upload image file

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -22,7 +22,7 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=100)
     excerpt = models.CharField(max_length=200)
-    img_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="post", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
